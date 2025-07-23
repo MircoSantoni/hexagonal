c196e261c3417f9ba2114e0d8e9b043fabaa47d3
mircosantoni <mirco.santoni@bizland.tech>
2025-07-23 12:07:15 -0300

    Update AppConfiguration.java

--- src/main/java/com/hexagonal/hexagonal/infrastructure/configuration/AppConfiguration.java
+++ src/main/java/com/hexagonal/hexagonal/infrastructure/configuration/AppConfiguration.java
@@ -1,12 +1,13 @@
 package com.hexagonal.hexagonal.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.hexagonal.hexagonal.application.ports.in.ClientServicePort;
 import com.hexagonal.hexagonal.application.ports.out.ClientPersistencePort;
 import com.hexagonal.hexagonal.application.ports.usecases.ClientServicePortImpl;
 
-
+@Configuration
 public class AppConfiguration {
 
     @Bean
