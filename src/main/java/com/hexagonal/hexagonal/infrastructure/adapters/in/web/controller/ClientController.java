b5b6dc4b51258445f61472d39d5206707341d440
mircosantoni <mirco.santoni@bizland.tech>
2025-07-24 09:11:06 -0300

    Update controller
    Update on routes and put and delete implementation

--- src/main/java/com/hexagonal/hexagonal/infrastructure/adapters/in/web/controller/ClientController.java
+++ src/main/java/com/hexagonal/hexagonal/infrastructure/adapters/in/web/controller/ClientController.java
@@ -3,9 +3,11 @@ package com.hexagonal.hexagonal.infrastructure.adapters.in.web.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,21 +17,19 @@ import com.hexagonal.hexagonal.infrastructure.adapters.in.web.dto.ClientDto;
 import com.hexagonal.hexagonal.infrastructure.adapters.in.web.dto.CreateClientCommand;
 import com.hexagonal.hexagonal.infrastructure.mapper.ClientEntityMapper;
 
-import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/clients")
+@RequestMapping("/api/clients/")
 public class ClientController {
 
 
     private final ClientServicePort clientServicePort;
     private final ClientEntityMapper clientEntityMapper;
 
-    // public ClientController(ClientServicePort clientServicePort, ClientEntityMapper clientEntityMapper) {
-    //     this.clientServicePort = clientServicePort;
-    //     this.clientEntityMapper = clientEntityMapper;
-    // }
+    public ClientController(ClientServicePort clientServicePort, ClientEntityMapper clientEntityMapper) {
+        this.clientServicePort = clientServicePort;
+        this.clientEntityMapper = clientEntityMapper;
+    }
 
     
     @GetMapping
@@ -37,7 +37,7 @@ public class ClientController {
         return ResponseEntity.ok(clientEntityMapper.fromListDomainToDtoList(clientServicePort.listAll()));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<ClientDto> findbyId(@PathVariable Long id) {
         return ResponseEntity.ok(clientEntityMapper.fromDomainToDto(clientServicePort.findById(id)));
     }
@@ -45,6 +45,16 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<ClientDto> createClient(@RequestBody CreateClientCommand createClientCommand) {
         return ResponseEntity.ok(clientEntityMapper.fromDomainToDto(clientServicePort.createClient(createClientCommand)));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ClientDto> updateCliEntity(@PathVariable Long id, @RequestBody CreateClientCommand createClientCommand) {        
+        return ResponseEntity.ok(clientEntityMapper.fromDomainToDto(clientServicePort.updateClient(createClientCommand, id)));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<ClientDto> deleteClient(@PathVariable Long id) {
+        return ResponseEntity.ok(clientEntityMapper.fromDomainToDto(clientServicePort.deleteClient(id)));
     }
 
 }
